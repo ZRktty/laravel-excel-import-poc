2a2dbd05e4327fe9059092c250d78a52e1d2a5c6
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2024-11-23 17:55:52 +0000

    make Product model categorizable, and minor improvements

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Rinvex\Categories\Traits\Categorizable;
 
 class Product extends Model
 {
+    use Categorizable;
+
+    /** The attributes that should be handled as date or datetime.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at', 'updated_at', 'deleted_at',
+    ];
+
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'brand_id',
+        'category_id',
+        'packaging',
+        'ean',
+        'on_sale',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'og_data'   => 'array',
+        'meta_data' => 'array',
+        'is_active' => 'boolean',
+        'on_sale' => 'boolean',
+    ];
+
+
     /**
      * Get the brand that owns the product.
      */
