b189c0e6cd21aea236e39d2c834fb7d78b4477d0
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2024-11-25 17:53:27 +0000

    config excel> chunk 100 and illuminate

--- config/excel.php
+++ config/excel.php
@@ -15,7 +15,7 @@ return [
         | Here you can specify how big the chunk should be.
         |
         */
-        'chunk_size'             => 1000,
+        'chunk_size'             => 100,
 
         /*
         |--------------------------------------------------------------------------
@@ -244,7 +244,7 @@ return [
         | Drivers: memory|illuminate|batch
         |
         */
-        'driver'      => 'memory',
+        'driver'      => 'illuminate',
 
         /*
         |--------------------------------------------------------------------------
