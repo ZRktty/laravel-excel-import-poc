d6782fb23d2d3dd6620263bc53844214f0e7f932
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2024-11-25 17:40:51 +0000

    chore: learn and test Category handling, attaching to product

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -2,12 +2,102 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
-
+use Rinvex\Categories\Models\Category;
 
 class DevController extends Controller
 {
-    public function test() {
-        return 'test';
+    public function createACategory()
+    {
+
+        $attributes = [
+            'name' => 'Electronics',
+            'slug' => 'electronics',
+            'description' => 'Electronics Category',
+            'icon' => 'fa fa-laptop',
+            'color' => '#000000',
+            'parent_id' => null,
+        ];
+        $category = app('rinvex.categories.category')->fill($attributes);
+        $category->save(); // Saved as root
+
+        return response()->json($category);
     }
+
+    public function getAllCategories()
+    {
+        $categories = Category::all();
+        return response()->json($categories);
+    }
+
+    public function appendToCategory()
+    {
+        $category = Category::find(1);
+        $category->children()->create([
+            'name' => 'Mobile Phones',
+            'slug' => 'mobile-phones',
+            'description' => 'Mobile Phones Category'
+        ]);
+
+        return response()->json($category);
+    }
+
+    public function createFromTree()
+    {
+        $newCategoryTree = app('rinvex.categories.category')->create([
+            'name' => [
+                'en' => 'Headphones',
+            ],
+
+            'children' => [
+                [
+                    'name' => 'Over-ear headphones',
+
+                    'children' => [
+                        ['name' => 'Wireless'],
+                    ],
+                ],
+            ],
+        ]);
+
+        $rootCategory = Category::find(1);
+        // #2 Using parent category
+        $rootCategory->appendNode($newCategoryTree);
+
+        return response()->json($rootCategory);
+
+    }
+
+    public function getAncestors($id)
+    {
+        $result = app('rinvex.categories.category')->ancestorsOf($id);
+        return response()->json($result);
+    }
+
+    public function tree()
+    {
+        $tree = Category::get()->toTree();
+        return response()->json($tree);
+    }
+
+    public function attachCategoryToProduct($productId, $categoryId)
+    {
+        $product = Product::find($productId);
+        $category = Category::find($categoryId);
+
+        if (!$product) {
+            return response()->json(['error' => 'Product not found'], 404);
+        }
+
+        if (!$category) {
+            return response()->json(['error' => 'Category not found'], 404);
+        }
+
+        // Single category id
+        $product->attachCategories($category->id);
+        return response()->json($product);
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,16 @@ Route::get('/', function () {
 });
 
 Route::get('/test', [DevController::class, 'test']);
+Route::get('/categories', [DevController::class, 'getAllCategories']);
+Route::get('/append-category', [DevController::class, 'appendToCategory']);
+
+Route::get('/append-tree', [DevController::class, 'createFromTree']);
+
+
+Route::get('/get-ancestors/{id}', [DevController::class, 'getAncestors']);
+//create  route for tree
+Route::get('/tree', [DevController::class, 'tree']);
+
+//route to attachCategoryToProduct
+Route::get('/attach-category-to-product/{productId}/{categoryId}', [DevController::class, 'attachCategoryToProduct']);
+
